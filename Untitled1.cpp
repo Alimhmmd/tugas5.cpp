25bab6b2612e98d315a1dfca84e78d6bab8fd8d1
ALI <alimhmmd434@gmail.com>
2018-01-03 17:52:36 +0700

    memasukan nilai variable kuda

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -10,6 +10,24 @@ int main(){
 	cout << "MATRIX X :"<<endl;
 	int kuda1=0;
 	do{
-	
+		cout << a1c[kuda1]<<" | ";
+		kuda1++;
+	}while(kuda1<5);cout<<endl;
+	int kuda2=0;
+	do{
+		cout << a2c[kuda2]<<" | ";
+		kuda2++;
+	}while(kuda2<5);cout<<endl;
+		int kuda3=0;
+	do{
+		cout << a3c[kuda3]<<" | ";
+		kuda3++;
+	}while(kuda3<5);cout<<endl;
+	//r
+	double mc1=max(a1c[0],max(a2c[0],a3c[0])),
+	mc2=max(a1c[1],max(a2c[1],a3c[1])),
+	mc3=max(a1c[2],max(a2c[2],a3c[2])),
+	mc4=max(a1c[3],max(a2c[3],a3c[3])),
+	mc5=max(a1c[4],max(a2c[4],a3c[4]));
 }
 
